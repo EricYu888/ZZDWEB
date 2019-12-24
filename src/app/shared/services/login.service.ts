044d560e82d8ef185272f4d039928de938af1dad
Eric <yjp1314@126.com>
2019-12-24 15:56:34 +0800

    remove console

--- src/app/shared/services/login.service.ts
+++ src/app/shared/services/login.service.ts
@@ -6,7 +6,7 @@ export class LoginService {
     constructor(public http: HttpService) { }
 
     public login(params):Promise<any>{
-        console.log(params)
+       
         return this.http.post('login/Login',params);
         // return this.http.post('user/login',params);
     }
