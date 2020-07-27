146ae0aab9b0f927c349a09b9f2848da4275fc10
Eric <yjp1314@126.com>
2020-07-27 21:40:19 +0800

    修改连接字符串

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,13 +1,6 @@
 export const SESSION_STORAGE = {
   USER_INFO: 'user'
 }  
-export const DOMAIN = 'http://218.24.128.75:3356/api/services/app/';
-export const EXPORT_FILE_PATH = 'http://218.24.128.75:3356/ExportFiles/';
-export const PIC_FILE_PATH = 'http://218.24.128.75:3356';
-// export const DOMAIN = 'http://localhost:6234/api/services/app/';
-
-
-
-// export const DOMAIN = 'http://localhost:8080/api/services/app/';
-// export const EXPORT_FILE_PATH = 'http://localhost:8080/ExportFiles/';
-// export const PIC_FILE_PATH = 'http://localhost:8080';
+export const DOMAIN = '';
+export const EXPORT_FILE_PATH = '';
+export const PIC_FILE_PATH = '';
